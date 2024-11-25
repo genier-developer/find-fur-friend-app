8deb46366817d21fdabaef29ae4c538b2b7f3eb1
Olga Genier <genier.developer@gmail.com>
2024-11-25 16:25:46 +0500

    update: React-Router v.7

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,3 +1,11 @@
 module.exports = {
-    extends: '@it-incubator/stylelint-config',
-}
+    extends: [
+        "stylelint-config-standard",
+        "stylelint-config-recommended-scss",
+    ],
+    rules: {
+        "block-no-empty": true,
+        "color-no-invalid-hex": true,
+        "declaration-block-no-duplicate-properties": true,
+    },
+};
--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
-import { ComponentType, lazy, Suspense } from 'react'
 import { useSelector } from 'react-redux'
+import { lazy, ReactNode, Suspense } from 'react'
 import {
   Navigate,
   Outlet,
@@ -11,29 +11,82 @@ import {
 import { selectUser } from '@/features/user/slices/auth-slice'
 import { LinearProgress } from '@mui/material'
 
-function LazyComponent<T>(
-  importFunc: () => Promise<{ default: ComponentType<T> }>
-): ComponentType<T> {
-  const Component = lazy(importFunc)
-  return (props: T) => (
-    <Suspense
-      fallback={
-        <div>
-          <LinearProgress />
-        </div>
-      }
-    >
-      <Component {...(props as any)} />
-    </Suspense>
-  )
-}
+const AddNewPet = lazy(() => import('@/features/pet/components/pet-form'))
+const Page404 = lazy(() => import('./pages/error-page'))
+const SignIn = lazy(() => import('./pages/sign-in'))
+const SignUp = lazy(() => import('./pages/sign-up'))
+const FavoritePetList = lazy(() => import('./pages/favorite-pets'))
+const HomePage = lazy(() => import('./pages/home-page'))
 
-const AddNewPet = LazyComponent(() => import('@/features/pet/components/pet-form'))
-const Page404 = LazyComponent(() => import('./pages/error-page'))
-const SignIn = LazyComponent(() => import('./pages/sign-in'))
-const SignUp = LazyComponent(() => import('./pages/sign-up'))
-const FavoritePetList = LazyComponent(() => import('./pages/favorite-pets'))
-const HomePage = LazyComponent(() => import('./pages/home-page'))
+const withSuspense = (Component: ReactNode) => (
+  <Suspense
+    fallback={
+      <div>
+        <LinearProgress />
+      </div>
+    }
+  >
+    {Component}
+  </Suspense>
+)
+
+const publicRoutes: RouteObject[] = [
+  {
+    element: withSuspense(<HomePage />),
+    index: true,
+    path: '/',
+  },
+  {
+    element: withSuspense(<SignIn />),
+    path: '/login',
+  },
+  {
+    element: withSuspense(<SignUp />),
+    path: '/signup',
+  },
+]
+
+const privateRoutes: RouteObject[] = [
+  {
+    element: withSuspense(<FavoritePetList />),
+    path: '/favorites',
+  },
+  {
+    element: withSuspense(<AddNewPet />),
+    path: '/add',
+  },
+]
+
+export const router = createBrowserRouter(
+  [
+    {
+      children: privateRoutes,
+      element: <PrivateRoutes />,
+    },
+    {
+      children: publicRoutes,
+      element: <PublicRoutes />,
+    },
+    {
+      element: withSuspense(<Page404 />),
+      path: '/404',
+    },
+    {
+      element: <Navigate to={'/404'} />,
+      path: '*',
+    },
+  ],
+  {
+    future: {
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+      v7_startTransition: true,
+    },
+  }
+)
 
 function PrivateRoutes() {
   const currentUser = useSelector(selectUser)
@@ -44,40 +97,6 @@ function PublicRoutes() {
   return <Outlet />
 }
 
-const routes: RouteObject[] = [
-  {
-    path: '/',
-    element: <PublicRoutes />,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: 'login', element: <SignIn /> },
-      { path: 'signup', element: <SignUp /> },
-    ],
-  },
-  {
-    path: '/',
-    element: <PrivateRoutes />,
-    children: [
-      { path: 'favorites', element: <FavoritePetList /> },
-      { path: 'add', element: <AddNewPet /> },
-    ],
-  },
-  { path: '404', element: <Page404 /> },
-  { path: '*', element: <Navigate to="/404" /> },
-]
-
-// Create router with Future Flags (for v.7)
-export const router = createBrowserRouter(routes, {
-  future: {
-    v7_relativeSplatPath: true,
-    v7_fetcherPersist: true,
-    v7_normalizeFormMethod: true,
-    v7_partialHydration: true,
-    v7_skipActionErrorRevalidation: true,
-  },
-})
-
-// Компонент Router
 export const Router = () => {
   return <RouterProvider router={router} />
 }
