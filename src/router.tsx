231e4a5f1743ea43ee5e71e5f56419b7ac96c26e
Olga Genier <genier.developer@gmail.com>
2024-11-25 11:15:17 +0500

    refactor: style

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
+import { ComponentType, lazy, Suspense } from 'react'
 import { useSelector } from 'react-redux'
-import { lazy, Suspense } from 'react'
 import {
   Navigate,
   Outlet,
@@ -11,69 +11,11 @@ import {
 import { selectUser } from '@/features/user/slices/auth-slice'
 import { LinearProgress } from '@mui/material'
 
-const AddNewPet = lazy(() => import('@/features/pet/components/pet-form'))
-const Page404 = lazy(() => import('./pages/error-page'))
-const SignIn = lazy(() => import('./pages/sign-in'))
-const SignUp = lazy(() => import('./pages/sign-up'))
-const FavoritePetList = lazy(() => import('./pages/favorite-pets'))
-const HomePage = lazy(() => import('./pages/home-page'))
-const publicRoutes: RouteObject[] = [
-  {
-    element: <HomePage />,
-    index: true,
-    path: '/',
-  },
-  {
-    element: <SignIn />,
-    path: '/login',
-  },
-  {
-    element: <SignUp />,
-    path: '/signup',
-  },
-]
-
-const privateRoutes: RouteObject[] = [
-  {
-    element: <FavoritePetList />,
-    path: '/favorites',
-  },
-  {
-    element: <AddNewPet />,
-    path: '/add',
-  },
-]
-
-export const router = createBrowserRouter([
-  {
-    children: privateRoutes,
-    element: <PrivateRoutes />,
-  },
-  {
-    children: publicRoutes,
-    element: <PublicRoutes />,
-  },
-  {
-    element: <Page404 />,
-    path: '/404',
-  },
-  {
-    element: <Navigate to={'/404'} />,
-    path: '*',
-  },
-])
-
-function PrivateRoutes() {
-  const currentUser = useSelector(selectUser)
-
-  return currentUser ? <Outlet /> : <Navigate to={'/login'} />
-}
-function PublicRoutes() {
-  return <Outlet />
-}
-
-export const Router = () => {
-  return (
+function LazyComponent<T>(
+  importFunc: () => Promise<{ default: ComponentType<T> }>
+): ComponentType<T> {
+  const Component = lazy(importFunc)
+  return (props: T) => (
     <Suspense
       fallback={
         <div>
@@ -81,7 +23,61 @@ export const Router = () => {
         </div>
       }
     >
-      <RouterProvider router={router} />
+      <Component {...(props as any)} />
     </Suspense>
   )
+}
+
+const AddNewPet = LazyComponent(() => import('@/features/pet/components/pet-form'))
+const Page404 = LazyComponent(() => import('./pages/error-page'))
+const SignIn = LazyComponent(() => import('./pages/sign-in'))
+const SignUp = LazyComponent(() => import('./pages/sign-up'))
+const FavoritePetList = LazyComponent(() => import('./pages/favorite-pets'))
+const HomePage = LazyComponent(() => import('./pages/home-page'))
+
+function PrivateRoutes() {
+  const currentUser = useSelector(selectUser)
+  return currentUser ? <Outlet /> : <Navigate to={'/login'} />
+}
+
+function PublicRoutes() {
+  return <Outlet />
+}
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <PublicRoutes />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'login', element: <SignIn /> },
+      { path: 'signup', element: <SignUp /> },
+    ],
+  },
+  {
+    path: '/',
+    element: <PrivateRoutes />,
+    children: [
+      { path: 'favorites', element: <FavoritePetList /> },
+      { path: 'add', element: <AddNewPet /> },
+    ],
+  },
+  { path: '404', element: <Page404 /> },
+  { path: '*', element: <Navigate to="/404" /> },
+]
+
+// Create router with Future Flags (for v.7)
+export const router = createBrowserRouter(routes, {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+})
+
+// Компонент Router
+export const Router = () => {
+  return <RouterProvider router={router} />
 }
