4d6800ed635129666eddd3315200909187516c24
Olga Genier <genier.developer@gmail.com>
2024-11-26 19:44:34 +0500

    refactor: style

--- src/features/user/auth-details.tsx
+++ src/features/user/auth-details.tsx
@@ -1,27 +1,14 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
-import { selectUser, signOutUser } from '@/features/user/slices/auth-slice'
-import { auth } from '@/services/firebase'
-import Button from '@mui/material/Button'
+import { selectUser } from '@/features/user/slices/auth-slice'
 import Typography from '@mui/material/Typography'
-import { signOut } from 'firebase/auth'
 
 export const AuthDetails = () => {
-  const dispatch = useDispatch()
   const user = useSelector(selectUser)
 
-  const userSignOut = () => {
-    signOut(auth)
-      .then(() => {
-        dispatch(signOutUser())
-      })
-      .catch(error => console.log(error))
-  }
-
   return (
-    <div>
-      <Button onClick={userSignOut}>Sign Out</Button>
-      <Typography variant={'h6'}>{`Signed in ${user?.email}`}</Typography>
-    </div>
+    <Typography variant={'subtitle1'} sx={{ marginLeft: 4 }}>
+      {`Signed as ${user?.email}`}
+    </Typography>
   )
 }
