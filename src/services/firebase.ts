c6fc37a63edc5bc6aa355139f5ee7e759760181f
Olga Genier <genier.developer@gmail.com>
2024-12-01 10:13:06 +0500

    fix: delete homepage in package.json

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAVxBJijswcwsg4DNsGtr53hdH07uAcxtA',
-  appId: '1:531819954511:web:c1805dddcc60457c3f853a',
-  authDomain: 'pet-shelter-fa4be.firebaseapp.com',
-  databaseURL: 'https://pet-shelter-fa4be-default-rtdb.firebaseio.com',
-  messagingSenderId: '531819954511',
-  projectId: 'pet-shelter-fa4be',
-  storageBucket: 'pet-shelter-fa4be.appspot.com',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
 }
 
 const app = initializeApp(firebaseConfig)
