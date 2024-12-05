28c2a2c1de18bcf3fb9a86988c1793a3bf68d4a2
Olga Genier <genier.developer@gmail.com>
2024-12-05 19:15:11 +0500

    refactor: avoid double auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { listenToAuthChanges } from '@/features/user/listen-to-auth-changes'
 import { store } from '@/app/store'
 import { Router } from './router'
 
-export const App = () => {
-  store.dispatch(listenToAuthChanges())
+store.dispatch(listenToAuthChanges())
 
+export const App = () => {
   return (
     <Provider store={store}>
       <Router />
